d9a055eaed521ce825ee81a9e758855a649bf1f5
zouyongsheng <zouyongsheng@kuaishou.com>
2023-01-30 19:49:56 +0800

    feat: 修改请求方式

--- src/api.ts
+++ src/api.ts
@@ -1,98 +1,94 @@
-import axios from 'axios';
-import { API_CODE } from './ui/type'
-import { getCookies, safeParse } from './utils';
+import axios from "axios";
+import { API_CODE } from "./ui/type";
+import { getCookies, safeParse } from "./utils";
 
 const cookies = getCookies();
-const userBasicInfo = safeParse(cookies['mybricks-login-user'])
+const userBasicInfo = safeParse(cookies["mybricks-login-user"]);
 
-type Namespace = 'system' | string
+type Namespace = "system" | string;
 
-export * from './apis';
+export * from "./apis";
 
 export const user = {
   getUserInfo: () => {
-    return axios({ 
-      method: 'get', 
-      url: `/paas/api/user/queryBy?email=${userBasicInfo.email}`
+    return axios({
+      method: "get",
+      url: `/paas/api/user/queryBy?email=${userBasicInfo.email}`,
     }).then(({ data }) => {
       if (data?.data) {
-        return data?.data?.[0]
+        return data?.data?.[0];
       } else {
-        throw new Error('获取用户信息失败失败')
+        throw new Error("获取用户信息失败失败");
       }
-    })
+    });
   },
   getLoginUser: () => {
-    return axios({ 
-      method: 'get', 
-      url: `/paas/api/user/signed`
+    return axios({
+      method: "post",
+      url: `/paas/api/user/signed`,
     }).then(({ data }) => {
       if (data?.data) {
-        return data?.data?.[0]
+        return data?.data?.[0];
       } else {
-        throw new Error('获取用户信息失败失败')
+        throw new Error("获取用户信息失败失败");
       }
-    })
-  }
-}
+    });
+  },
+};
 
 export const setting = {
   getSetting: (namespaces: Namespace[]) => {
-    return axios({ 
-      method: 'post', 
-      url: '/api/config/get',
+    return axios({
+      method: "post",
+      url: "/api/config/get",
       data: {
-        scope: namespaces
-      }
+        scope: namespaces,
+      },
     }).then(({ data }) => {
       if (data?.data) {
-        return data?.data
+        return data?.data;
       } else {
-        throw new Error('获取设置失败')
+        throw new Error("获取设置失败");
       }
-    })
+    });
   },
   saveSetting: (namespace: string, config: any) => {
-    return axios({ 
-      method: 'post', 
-      url: '/api/config/update',
+    return axios({
+      method: "post",
+      url: "/api/config/update",
       data: {
         namespace: namespace,
         userId: userBasicInfo.email,
         config,
-      }
+      },
     }).then(({ data }) => {
       const { code } = data ?? {};
       if (code === API_CODE.SUCCESS) {
-        return true
+        return true;
       } else {
-        throw new Error('保存系统设置失败')
+        throw new Error("保存系统设置失败");
       }
-    })
-  }
-}
+    });
+  },
+};
 
 export const task = {
-  exec: ({
-    fileId,
-    version,
-  }: { fileId: string, version: string }, params) => {
-    return axios({ 
-      method: 'post', 
-      url: '/api/system/task/run',
+  exec: ({ fileId, version }: { fileId: string; version: string }, params) => {
+    return axios({
+      method: "post",
+      url: "/api/system/task/run",
       data: {
         fileId,
         version,
-        injectParam: JSON.stringify(params)
-      }
+        injectParam: JSON.stringify(params),
+      },
     }).then(({ data }) => {
       const { code, msg, message } = data ?? {};
       if (code === API_CODE.SUCCESS) {
-        return true
+        return true;
       } else {
-        throw new Error(msg || message || '执行任务失败')
+        throw new Error(msg || message || "执行任务失败");
       }
-    })  
-  }
-}
-
+    });
+  },
+};
