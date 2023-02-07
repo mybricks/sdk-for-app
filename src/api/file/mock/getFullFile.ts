d0e57f48b0143b250c6714cb76adc8b562734363
leon-js <42924541+leon-js@users.noreply.github.com>
2023-02-07 19:52:53 +0800

    fix:mock file getFullFile

--- src/api/file/mock/getFullFile.ts
+++ src/api/file/mock/getFullFile.ts
@@ -5,7 +5,10 @@ const getFullFile: typeof fn = (params) => {
   return new Promise((resolve) => {
     const localKey = `${LOCAL_APP_DUMPJSON_KEY_PREFIX}_${params.fileId}`;
     
-    resolve(JSON.parse(window.localStorage.getItem(localKey) || "{}"));
+    resolve(Object.assign({...JSON.parse(window.localStorage.getItem(localKey) || "{}")}, {
+      creatorId: "dev@mock.com",
+      creatorName: "dev"
+    }));
   })
 }
 
