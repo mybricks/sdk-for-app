f5773ea0079a4fadb07d39f1c0fdcfc36b509519
leon-js <42924541+leon-js@users.noreply.github.com>
2023-02-07 20:42:09 +0800

    fix:去除console

--- src/api/file/mock/getFullFile.ts
+++ src/api/file/mock/getFullFile.ts
@@ -4,7 +4,7 @@ import { LOCAL_APP_DUMPJSON_KEY_PREFIX } from "./const";
 const getFullFile: typeof fn = (params) => {
   return new Promise((resolve) => {
     const localKey = `${LOCAL_APP_DUMPJSON_KEY_PREFIX}_${params.fileId}`;
-    
+
     resolve(Object.assign({...JSON.parse(window.localStorage.getItem(localKey) || "{}")}, {
       creatorId: "dev@mock.com",
       creatorName: "dev"
