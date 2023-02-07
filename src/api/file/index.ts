bf4cc687077caa802280024816c802d0e6bece5c
zouyongsheng <zouyongsheng@kuaishou.com>
2023-02-07 17:57:42 +0800

    feat: 添加save

--- src/api/file/index.ts
+++ src/api/file/index.ts
@@ -2,10 +2,12 @@ import getAll from "./getAll";
 import getPublishContent from './getPublishContent'
 import getVersions from './getVersions'
 import getFullFile from './getFullFile'
+import save from './save'
 
 export const File = {
   getFullFile,
   getAll,
+  save,
   getPublishContent,
   getVersions,
 }
---
+++ src/api/file/save.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const getAll = (params: {userId, fileId, shareType, name, content, icon, namespace, type}): Promise<any[]> => {
+  return new Promise((resolve, reject) => {
+    axios({ 
+      method: 'get', 
+      url: '/paas/api/workspace/saveFile',
+      params: params
+    }).then(({ data }) => {
+      if (data?.code === 1) {
+        resolve(data?.data)
+      } else {
+        reject('保存失败')
+      }
+    })
+  })
+}
+
+export default getAll
