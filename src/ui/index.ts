87548e3415a93bbee24ba36a5d2ed4d00d337928
zouyongsheng <zouyongsheng@kuaishou.com>
2023-03-29 10:37:24 +0800

    feat: update

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,9 +2,6 @@ import View from './view'
 import Toolbar from './toolbar'
 import Locker from './locker'
 import { openFilePanel } from './openFilePanel/openPanel'
-import pkg from '../../package.json'
-
-console.log(`%c ${pkg.name} %c@${pkg.version}`, "color:#FFF;background:#fa6400", "", "")
 
 export {
   View, Toolbar, openFilePanel,Locker
