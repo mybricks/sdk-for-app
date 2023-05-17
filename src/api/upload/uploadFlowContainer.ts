aef8a847f76cb7951242992fc9b734fbb0a41790
zouyongsheng <zouyongsheng@kuaishou.com>
2023-05-17 10:46:20 +0800

    feat: 升级版本

--- src/api/upload/uploadFlowContainer.ts
+++ src/api/upload/uploadFlowContainer.ts
@@ -1,10 +1,10 @@
 // @ts-ignore
 import { getAxiosInstance } from '../util'
 
-export default function uploadFlowContainer({ projectId, codeStrList }: { projectId: number, codeStrList: { fileId: number, fileName: string, content: string}[] }) {
+export default function uploadFlowContainer(params: { projectId: number, codeStrList: { fileId: number, fileName: string, content: string}[], envType: 'staging' | 'prod' }) {
   return new Promise((resolve, reject) => {
     getAxiosInstance()
-    .post('/paas/api/flow/file/batchCreate', { projectId, codeStrList })
+    .post('/paas/api/flow/file/batchCreate', params)
     .then(({ data }: any) => {
       if (data.code === 1 && data.data) {
         resolve(data.data)
