be3ed16e8b35d4f9b77f7f1059d1b2a5354c5171
liubiao03 <liubiao03@kuaishou.com>
2023-06-25 16:29:17 +0800

    feat: support comlib debugger

--- src/api/env.ts
+++ src/api/env.ts
@@ -12,6 +12,19 @@ export function isEnvOfServer() {
   return typeof process !== 'undefined'
 }
 
+/** 是否处于调试组件库模式 */
+export function isDebugComlibMode() {
+  if (!window?.location || !window?.URL) {
+    return false
+  }
+  return new URL(window.location?.href)?.searchParams?.get?.('bricks_debug_server')
+}
+
 export function isEnvOfDevAndBrowser() {
+  /** 组件库调试模式走mock */
+  if (isDebugComlibMode()) {
+    return true
+  }
+
   return isEnvOfDev() && isEnvOfBrowser()
 }
