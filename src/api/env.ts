584716b5ebdff2147bc5a3a31d9799415cc68e63
zouyongsheng <zouyongsheng@kuaishou.com>
2023-06-25 20:32:22 +0800

    feat: 暂时注释服务端运行错误代码

--- src/api/env.ts
+++ src/api/env.ts
@@ -13,18 +13,18 @@ export function isEnvOfServer() {
 }
 
 /** 是否处于调试组件库模式 */
-export function isDebugComlibMode() {
-  if (!window?.location || !window?.URL) {
-    return false
-  }
-  return new URL(window.location?.href)?.searchParams?.get?.('bricks_debug_server')
-}
+// export function isDebugComlibMode() {
+//   if (!window?.location || !window?.URL) {
+//     return false
+//   }
+//   return new URL(window.location?.href)?.searchParams?.get?.('bricks_debug_server')
+// }
 
 export function isEnvOfDevAndBrowser() {
-  /** 组件库调试模式走mock */
-  if (isDebugComlibMode()) {
-    return true
-  }
+  // /** 组件库调试模式走mock */
+  // if (isDebugComlibMode()) {
+  //   return true
+  // }
 
   return isEnvOfDev() && isEnvOfBrowser()
 }
