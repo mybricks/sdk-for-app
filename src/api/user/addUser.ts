dde96a39e4b63c39dd9c32aea361d61be4f1524b
zouyongsheng <zouyongsheng@kuaishou.com>
2023-08-04 12:10:17 +0800

    feat: update

--- src/api/user/addUser.ts
+++ src/api/user/addUser.ts
@@ -2,20 +2,16 @@ import { getAxiosInstance } from '../util'
 
 /**
  * 注册用户
- * 
+ *
  * @param {{name: string, email: string, avatar: string}} params
- * @returns {Promise<{ userId: number }>}
+ * @returns {(Promise<{ code: 1 | -1, data: { userId: number } }>)}
  */
-function addUser(params: {name: string, email: string, avatar: string}): Promise<{ userId: number }> {
+function addUser(params: {name: string, email: string, avatar: string}): Promise<{ code: 1 | -1, data: { userId: number } }> {
   return new Promise((resolve, reject) => {
     getAxiosInstance()
     .post('/paas/api/user/addUser', params)
     .then(({data}: any) => {
-      if (data?.code === 1) {
-        resolve(data?.data);
-      } else {
-        reject('注册用户失败');
-      }
+      resolve(data)
     }).catch((e: any) => {
       reject(e.msg || '注册用户失败')
     })
