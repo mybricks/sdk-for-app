d0eed9ce42ecfd0bfe9cdf151cbe7baf479e1791
andyzys <ml_zys@126.com>
2023-10-26 16:50:29 +0800

    feat: 增加新接口

---
+++ src/api/upload/saveProducts.ts
@@ -0,0 +1,41 @@
+// @ts-ignore
+import { getAxiosInstance } from '../util'
+import { isEnvOfServer } from '../env'
+
+/**
+ * 保存每个版本的制品到存储中
+ *
+ * @param {{ content: any, fileId: number, type: string, version: string }} param
+ * @returns
+ */
+function saveProducts(param: { content: any, fileId: number, type: string, version: string }) {
+  const {content, fileId, type, version} = param
+  let blob;
+  let formData: any;
+  if(isEnvOfServer()) {
+    // @ts-ignore
+    formData = param
+  } else {
+    blob = new Blob([content])
+    // fix 客户端调用会报错
+    formData = new window.FormData()
+    formData.append('file', blob, `${fileId}.zip`)
+    formData.append('fileId', fileId)
+    formData.append('type', type)
+    formData.append('version', version)
+  }
+
+  return new Promise((resolve, reject) => {
+    getAxiosInstance()
+    .post('/paas/api/flow/saveProducts', formData)
+    .then(({ data }: any) => {
+      if (data.code === 1 && data.data) {
+        resolve(data.data)
+      } else {
+        reject('上传失败')
+      }
+    })
+  })
+}
+
+export default saveProducts
