b2dae43680d940e3aa6351046b17cb159d956f70
andyzys <ml_zys@126.com>
2023-10-26 16:50:29 +0800

    feat: 增加新接口

--- src/api/upload/index.ts
+++ src/api/upload/index.ts
@@ -1,9 +1,11 @@
 import staticServer from './staticServer'
 import uploadFlowContainer from './uploadFlowContainer'
 import toOss from './toOss'
+import saveProducts from './saveProducts'
 
 const Upload = {
   staticServer,
+  saveProducts,
   toOss,
   uploadFlowContainer
 }
