a3efd8045b86e86bd4c20233423433cc79071f64
andyzys <ml_zys@126.com>
2023-11-08 21:36:51 +0800

    feat: 增加注释

--- src/api/material/getMaterialContent.ts
+++ src/api/material/getMaterialContent.ts
@@ -1,6 +1,12 @@
 import { getAxiosInstance } from '../util'
 
-const getMaterialContent = (params: { namespace: string; version?: string; codeType?: string }): Promise<any> => {
+/**
+ * 获取物料内容
+ *
+ * @param {{ namespace: string; version?: string; codeType?: string }} params
+ * @returns {Promise<any>}
+ */
+function getMaterialContent(params: { namespace: string; version?: string; codeType?: string }): Promise<any> {
 	return new Promise((resolve, reject) => {
 		getAxiosInstance()
 			.get('/api/material/namespace/content', { params })
