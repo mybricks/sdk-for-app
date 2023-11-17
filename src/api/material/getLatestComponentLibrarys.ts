6a48d6591c60b8e47a652f241e1c6bdbbefe31af
leon-js <42924541+leon-js@users.noreply.github.com>
2023-11-17 11:46:42 +0800

    feat:获取最新组件库接口根据查询顺序排序

--- src/api/material/getLatestComponentLibrarys.ts
+++ src/api/material/getLatestComponentLibrarys.ts
@@ -10,7 +10,9 @@ const getLatestComponentLibrarys = async (namespaces: Array<string>) => {
     })
     .then(({ data }: any) => {
       if (data?.data) {
-        resolve(data?.data || [])
+        resolve((data?.data || []).sort((p: any, c: any) => {
+          return namespaces.indexOf(p.namespace) - namespaces.indexOf(c.namespace);;
+        }))
       } else {
         reject('查询最新组件库信息失败')
       }
