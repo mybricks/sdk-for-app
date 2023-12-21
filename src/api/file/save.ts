0f15c1487bf5b73bcb6013dee1be64dab81a037b
andyzys <ml_zys@126.com>
2023-12-21 23:30:32 +0800

    feat: xiugai

--- src/api/file/save.ts
+++ src/api/file/save.ts
@@ -8,6 +8,11 @@ import { getAxiosInstance } from '../util'
  */
 function save(params: {userId: any, fileId: any, shareType?: any, name?: any, content: any, icon?: any, namespace?: any, type?: any}): Promise<{}> {
   return new Promise((resolve, reject) => {
+    // @ts-ignore
+    if(params?.isEncode && params?.content) {
+      console.log('开启压缩')
+      params.content = encodeURI(params?.content.replace(/\./g, '#D#').replace(/\,/g, '#DH#').replace(/\;/g, '#FH#').replace(/\(/g, '#ZKH#').replace(/\)/g, '#YKH#').replace(/\:/g, '#MH#').replace(/\'/g, '#DYH#'))
+    }
     getAxiosInstance()
     .post('/paas/api/workspace/saveFile', params)
     .then(({data}: any) => {
