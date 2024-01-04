ab3194349fe6c33a192c155852429c1a8d6ac4d6
zhuba-Ahhh <3477826311@qq.com>
2024-01-04 19:29:52 +0800

    feat(ui/toolbar): add disabled click event

--- src/ui/toolbar/button.tsx
+++ src/ui/toolbar/button.tsx
@@ -1,38 +1,45 @@
-import React from 'react'
+import React from "react";
 
-import { LoadingOutlined } from '@ant-design/icons'
+import { LoadingOutlined } from "@ant-design/icons";
 
-import css from './index.less'
+import css from "./index.less";
 
 interface SaveButtonProps {
-  disabled?: boolean
-  loading?: boolean
+  disabled?: boolean;
+  loading?: boolean;
   /** 右上角小红点提示可以保存 */
-  dotTip?: boolean
-  onClick?: () => void
-  children?: any
-  type?: 'primary'
+  dotTip?: boolean;
+  onClick?: () => void;
+  onDisabledClick?: () => void;
+  children?: any;
+  type?: "primary";
 }
 
 export default ({
   disabled = false,
   loading = false,
   onClick,
+  onDisabledClick,
   children,
   type,
 }: SaveButtonProps) => {
   return (
     <button
-      disabled={disabled}
-      className={`${css.button} ${type === 'primary' ? css.mainButton : ''} ${loading ? css.loading : ''}`}
+      className={`${css.button} ${type === "primary" ? css.mainButton : ""} ${loading ? css.loading : ""} ${disabled ? css.disabled : ""}`}
       onClick={() => {
         if (!loading) {
-          onClick?.()
+          if (disabled) {
+            if (typeof onDisabledClick === 'function'){
+              onDisabledClick?.();
+            }
+          } else {
+            onClick?.();
+          }
         }
       }}
     >
-      <LoadingOutlined style={{marginRight: 4, display: loading ? 'inline-block' : 'none'}}/>
+      <LoadingOutlined style={{ marginRight: 4, display: loading ? "inline-block" : "none" }} />
       {children}
     </button>
-  )
-}
+  );
+};
