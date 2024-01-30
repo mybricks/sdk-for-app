79ef1a28c8bf2d493404646a4b1cbb7162aae4dd
Leo <liulei.leo306@gmail.com>
2024-01-30 17:49:36 +0800

    feat: 内置替换逻辑

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,10 +1,13 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { INLINE_ASSETS } from './constant';
+const fs = require('fs');
 
 interface AssetMapPluginOption {
 	/** 文件名 */
 	filename: string;
 	/** 使用 chunk 名 */
 	chunks: string[];
+	template?: string;
 	/** 需要映射的资源 map */
 	assetsMap: Array<{ tag: string; path: string; CDN: string; }>;
 	/** 处理 template 字符串的方法 */
@@ -12,20 +15,22 @@ interface AssetMapPluginOption {
 }
 
 const generateAssetMapPlugin = (option: AssetMapPluginOption) => {
-	const { filename, chunks, assetsMap, templateContent } = option;
+	const { filename, chunks, assetsMap = [], template, templateContent } = option;
 
 	return [
 		new HtmlWebpackPlugin({
 			filename,
 			chunks,
-			templateContent: (param: any) => templateContent(param)
+			templateContent: (param: any) => {
+				return templateContent ? templateContent(param) : (template ? fs.readFileSync(template, 'utf-8') : '');
+			}
 		}),
 		new HtmlWebpackPlugin({
 			filename: filename.replace('.html', '.offline.html'),
 			chunks,
 			templateContent: (param: any) => {
-				let originContent = templateContent(param);
-				assetsMap.forEach(asset => {
+				let originContent = templateContent ? templateContent(param) : (template ? fs.readFileSync(template, 'utf-8') : '');
+				[...assetsMap, ...INLINE_ASSETS].forEach(asset => {
 					originContent = originContent.replace(new RegExp(`^${asset.path}$`, 'g'), asset.CDN);
 				});
 
