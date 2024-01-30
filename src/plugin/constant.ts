14e81b01d6ea1389c137a07dc0ea7bf8f68fec39
Leo <liulei.leo306@gmail.com>
2024-01-30 19:28:20 +0800

    feat: 增加内置 CDN 映射

--- src/plugin/constant.ts
+++ src/plugin/constant.ts
@@ -2,62 +2,67 @@ export const INLINE_ASSETS = [
 	{
 		tag: 'script',
 		path: 'public/moment/moment@2.29.4.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690443543399.2.29.4_moment.min.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690443543399.2.29.4_moment.min.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/moment/locale/zh-cn.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690443577599.2.29.4_locale_zh-cn.min.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690443577599.2.29.4_locale_zh-cn.min.js'
 	},
 	{
 		tag: 'link',
 		path: 'public/antd/antd@4.21.6.variable.min.css',
-		CDN: '//f2.eckwai.com/udata/pkg/eshop/fangzhou/pub/pkg/antd-4.21.6/antd.variable.min.css'
+		CDN: 'https://f2.eckwai.com/udata/pkg/eshop/fangzhou/pub/pkg/antd-4.21.6/antd.variable.min.css'
+	},
+	{
+		tag: 'link',
+		path: 'public/antd/antd@4.21.6.min.css',
+		CDN: 'https://f2.eckwai.com/kos/nlav12333/mybricks/antd4.21.6.min.css'
 	},
 	{
 		tag: 'script',
 		path: 'public/react@18.0.0.production.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690446345009.react.18.0.0.production.min.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690446345009.react.18.0.0.production.min.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/react-dom@18.0.0.production.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690443341846.umd_react-dom.production.min.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690443341846.umd_react-dom.production.min.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/antd/antd@4.21.6.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690444184854.4.21.6_antd.min.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690444184854.4.21.6_antd.min.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/antd/locale/zh_CN.js',
-		CDN: '//f2.eckwai.com/udata/pkg/eshop/fangzhou/pub/pkg/antd-4.21.6/locale/zh_CN.js'
+		CDN: 'https://f2.eckwai.com/udata/pkg/eshop/fangzhou/pub/pkg/antd-4.21.6/locale/zh_CN.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/ant-design-icons@4.7.0.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690444248634.ant-design-icons_4.7.0_min.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690444248634.ant-design-icons_4.7.0_min.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/ant-design-charts@1.3.5.min.js',
-		CDN: '//f2.beckwai.com/udata/pkg/eshop/fangzhou/pub/pkg/ant-design/charts-1.3.5/charts.min.js'
+		CDN: 'https://f2.beckwai.com/udata/pkg/eshop/fangzhou/pub/pkg/ant-design/charts-1.3.5/charts.min.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/plugin-http-connector/1.1.79/index.js',
-		CDN: '//f2.eckwai.com/kos/nlav12333/mybricks/plugin-http-connector/1.1.79/index.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav12333/mybricks/plugin-http-connector/1.1.79/index.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/mybricks-plugin-connector-domain@0.0.44.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690445635042.mybricks_plugin-connector-domain@0.0.44.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav11092/fangzhou/pub/temp/1690445635042.mybricks_plugin-connector-domain@0.0.44.js'
 	},
 	{
 		tag: 'script',
 		path: 'public/render-web/1.2.44/index.min.js',
-		CDN: '//f2.beckwai.com/kos/nlav12333/mybricks/render-web/1.2.44/index.min.js'
+		CDN: 'https://f2.beckwai.com/kos/nlav12333/mybricks/render-web/1.2.44/index.min.js'
 	},
 	{
 		tag: 'link',
@@ -77,6 +82,21 @@ export const INLINE_ASSETS = [
 	{
 		tag: 'script',
 		path: 'public/render-web-vue2/0.0.4/index.min.js',
-		CDN: '//f2.eckwai.com/kos/nlav12333/mybricks/render-web-vue2/0.0.4/index.min.js'
+		CDN: 'https://f2.eckwai.com/kos/nlav12333/mybricks/render-web-vue2/0.0.4/index.min.js'
 	},
+	{
+		tag: 'script',
+		path: 'public/axios@0.18.1.min.js',
+		CDN: 'https://f2.eckwai.com/kos/nlav12333/mybricks/axios0.18.1.min.js',
+	},
+	{
+		tag: 'script',
+		path: 'public/babel-standalone.min.js',
+		CDN: 'https://f2.eckwai.com/kos/nlav12333/mybricks/babel/babel-standalone.min.js',
+	},
+	{
+		tag: 'script',
+		path: 'public/json-schema-to-typescript.js',
+		CDN: 'https://f2.eckwai.com/kos/nlav12333/mybricks/babel/json-schema-to-typescript.js',
+	}
 ];
