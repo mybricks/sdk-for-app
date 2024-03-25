792373e26031d54b2cb2c41e2605553c90aaaf0b
andyzys <ml_zys@126.com>
2024-03-25 21:57:50 +0800

    feat: 增加接口

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,11 +1,13 @@
 import getUserInfo from "./getUserInfo";
 import getLoginUser from "./getLoginUser";
 import addUser from "./addUser";
+import searchUserByKeyword from './searchUserByKeyword'
 
 const User = {
   getUserInfo,
   getLoginUser,
-  addUser
+  addUser,
+  searchUserByKeyword
 }
 
 export default User
---
+++ src/api/user/searchUserByKeyword.ts
@@ -0,0 +1,32 @@
+import { getAxiosInstance } from '../util'
+import type { T_UserInfo } from "./type";
+
+/**
+ * 获取用户信息
+ *
+ * @param {string} email 用户邮箱
+ * @returns {Promise<T_UserInfo>}
+ */
+async function searchUserByKeyword({ keyword }: any): Promise<T_UserInfo> {
+  return new Promise((resolve, reject) => {
+    if (!keyword) {
+      reject('keyword不能为空');
+      return
+    }
+
+    getAxiosInstance()
+    .get('/paas/api/user/searchByKeyword', { keyword })
+    .then(({ data }: any) => {
+      if (data?.code === 1) {
+        resolve(data?.data)
+      } else {
+        reject('获取用户信息失败失败')
+      }
+    });
+  })
+
+
+}
+
+export default searchUserByKeyword
+
