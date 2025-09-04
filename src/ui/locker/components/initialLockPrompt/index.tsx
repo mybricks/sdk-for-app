3514ca23a0b9219d83aba2d2e16a659210cfc9fb
moyukai05 <moyukai05@kuaishou.com>
2025-09-04 17:01:13 +0800

    feat(ui):优化上锁提示弹窗

--- src/ui/locker/components/initialLockPrompt/index.tsx
+++ src/ui/locker/components/initialLockPrompt/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { notification } from "antd";
 import css from "./index.less";
+import {locker_header,locker_desc} from '../icons/index'
 
 const initialLockPrompt = (params) => {
   const { type, onClose } = params;
@@ -12,15 +13,20 @@ const initialLockPrompt = (params) => {
       className: "fangzhou-theme",
       top: 45,
       style: {
-        marginRight: -8
+        marginRight: 0,
+        paddingTop: 10,
+        paddingBottom: 18,
+        paddingLeft: 24,
+        paddingRight: 19,
+        borderRadius: 12,
       },
-      message: (<h2 className={css.h2}>请注意</h2>),
+      message: "",
       description: (
         <div className={css.notification}>
-          <div>当前应用未上锁</div>
-          <div className={css.name}>
-            - 如果需要编辑保存内容
-            <span className={css.reason}>请点击右上角头像上锁</span>
+          <div className={css.title}>需要上锁才能保存</div>
+          <div className={css.item}>
+            <div className={css.name}>应用未上锁</div>
+            <div className={css.reason}>请点击右上角头像上锁</div>
           </div>
         </div>
       ),
@@ -33,19 +39,29 @@ const initialLockPrompt = (params) => {
       className: "fangzhou-theme",
       top: 45,
       style: {
-        marginRight: -8
+        marginRight: 0,
+        paddingTop: 10,
+        paddingBottom: 13,
+        paddingLeft: 24,
+        paddingRight: 19,
+        borderRadius: 12,
       },
-      message: (<h2 className={css.h2}>请注意</h2>),
+      message: "",
       description: (
         <div className={css.notification}>
-          <div>当前没有应用和画布区块的编辑权限</div>
-          <div className={css.name}>
-            - 应用配置编辑
-            <span className={css.reason}>请点击右上角头像上锁</span>
+          <div className={css.title}>需要上锁才能保存</div>
+          <div className={css.item}>
+            <div className={css.name}>应用配置</div>
+            <div className={css.reason}>点击上方头像
+              <img src={locker_header} style={{height:"18px",width:"19px",margin:"0 4px"}} alt="" />
+             上锁</div>
           </div>
-          <div className={css.name}>
-            - 画布区块编辑
-            <span className={css.reason}>请点击左上角画布和区块申请权限</span>
+
+          <div className={css.item} style={{alignItems: 'start'}}>
+            <div className={css.name}>画布区块</div>
+            <div className={css.reason} style={{alignItems: 'start'}}>点击左侧画布和区块菜单
+              <img src={locker_desc} alt="锁图标" style={{width: '100px', height: 'auto', verticalAlign: 'middle', margin: '0 4px'}} />
+              上锁</div>
           </div>
         </div>
       ),
